7bf8faec291fa1577c42e1f167d98c9d9f524491
laurynasg <glaurynasg@gmail.com>
2021-02-04 14:44:32 +0200

    Add delete feature

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -4,10 +4,11 @@ import * as api from '../api';
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
+
         dispatch({ type: 'FETCH_ALL', payload: data });
     } catch (error) {
         console.log(error.message);
-    }
+    };
 };
 
 export const createPost = (post) => async (dispatch) => {
@@ -17,14 +18,25 @@ export const createPost = (post) => async (dispatch) => {
         dispatch({ type: 'CREATE', payload: data });
     } catch (error) {
         console.log(error.message);
-    }
+    };
 };
 
 export const updatePost = (id, post) => async (dispatch) => {
     try {
         const { data } = await api.updatePost(id, post);
+
         dispatch({ type: 'UPDATE', payload: data });
     } catch (error) {
         console.log(error.message);
-    }
+    };
+};
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+
+        dispatch({ type: 'DELETE', payload: id });
+    } catch (error) {
+        console.log(error.message);
+    };
 };
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -29,7 +29,17 @@ export const updatePost = async (req, res) => {
 
     if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('no post with that id');
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
 
     res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('no post with that id');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post deleted successfully' });
 };
