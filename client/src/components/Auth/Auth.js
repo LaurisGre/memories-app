ad43cb9050f68a66e0084d1cedbe5aaf80a88aa4
laurynasg <glaurynasg@gmail.com>
2021-02-05 13:34:58 +0200

    Eddit-C Auth component

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,10 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Input from './Input';
+
+import useStyles from './styles';
 
 const Auth = () => {
+    const classes = useStyles();
+    const [showPassword, setShowPassword] = useState(false);
+    const [isSignup, setIsSignup] = useState(false);
+
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    };
+
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+        handleShowPassword(false);
+    };
+
     return (
-        <div>
-            AUTH
-        </div>
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant="h5">
+                    {isSignup ? 'Sign Up' : 'Sign In'}
+                </Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {isSignup && (<>
+                            <Input name="firstname" label="First Name" handleChange={handleChange} autoFocus half />
+                            <Input name="lastname" label="Last Name" handleChange={handleChange} half />
+                        </>)}
+                        <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                        <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
+                        {isSignup && <Input name="confirmPassword" label="Reapeat Password" handleChange={handleChange} type="password" />}
+                    </Grid>
+                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                        {isSignup ? 'Sign Up' : 'Sign In'}
+                    </Button>
+                    <Grid container justify="space-around" alignItems="center">
+                        <Typography>
+                            {isSignup ? 'Already have an account?' : 'Don\'t have an account?'}
+                        </Typography>
+                        <Button onClick={switchMode}>
+                            {isSignup ? 'Sign In!' : ' Sign Up!'}
+                        </Button>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
     )
 }
 
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -13,25 +13,20 @@ const Navbar = () => {
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className={classes.brandContainer}>
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
                 <img className={classes.image} src={memories} alt="memories" height="60" />
             </div>
             <Toolbar className={classes.toolbar}>
                 {user ?
-                    (
-                        <div className={classes.profile}>
-                            <Avatar className={classes.purple} alt={user.result.name} src={user.resulr.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-                            <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                            <Button variant="contained" className={classes.logout} color="secondary" onClick={() => { }}>Logout</Button>
-                        </div>
-                    ) :
-                    (
-                        <Button component={Link} to="/auth" variant="contained" colort="primary">Sign in</Button>
-                    )
+                    (<div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.resulr.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                        <Button variant="contained" className={classes.logout} color="secondary" onClick={() => { }}>Logout</Button>
+                    </div>) :
+                    (<Button component={Link} to="/auth" variant="contained" colort="primary">Sign in</Button>)
                 }
             </Toolbar>
         </AppBar>
-
     );
 };
 
