a3f37147b487756c406f602e8ee339c213be8e6d
laurynasg <glaurynasg@gmail.com>
2021-02-09 18:50:56 +0200

    Edit like, delete and edit post features

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Button, Typography, Toolbar, Avatar } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-
+import decode from 'jwt-decode';
 import memories from '../../images/memories.png';
 import useStyles from './styles';
 
@@ -20,7 +20,13 @@ const Navbar = () => {
     }
 
     useEffect(() => {
-        // const token = user?.token;
+        const token = user?.token;
+
+        if (token) {
+            const decodedToken = decode(token);
+
+            if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+        };
 
         setUser(JSON.parse(localStorage.getItem('profile')));
     }, [location]);
